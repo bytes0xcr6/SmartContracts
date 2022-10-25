650f6361cbc8e8546878883b308961e864cc5542
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2022-10-25 16:33:19 +0200

    Create 1_initial_migration.js

---
+++ Vehicle Registration/Migration/1_initial_migration.js
@@ -0,0 +1,10 @@
+const Vehicle = artifacts.require("Vehicle");
+const RegisterVehicles = artifacts.require("RegisterVehicles");
+const Ownable = artifacts.require("Ownable");
+
+module.exports = function(deployer) {
+    deployer.deploy(Vehicle,2, true, "0xc5B46eaAb07Bdf4D28e3120694C98288b7c4cEBf", 1, "hola6", "Mercedes");
+    deployer.deploy(Ownable);
+    deployer.link(RegisterVehicles, Ownable);
+    deployer.deploy(RegisterVehicles);
+}
