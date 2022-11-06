38eac36c75f2eb3afb632f4be7c1c10ddbe769a6
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2022-11-06 14:41:05 +0100

    Update Script.js

--- Ethernaut-Challenges/Privacy/Script.js
+++ Ethernaut-Challenges/Privacy/Script.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
-const web3 = new Web3("https://polygon-mumbai.g.alchemy.com/v2/qyfgNG7RUCVvgv2yPnYwVzxfjFf0-cl7");
+const web3 = new Web3("https://polygon-mumbai.g.alchemy.com/v2/<API KEY>");
 
-const contract = "0x77aF4957D46ac0f2163CF3c9097A6c2EbD61A5f7";
+const contract = "<Contract address>";
 
 // It will return the slot 5th, which is the last position in the data array
 web3.eth.getStorageAt(contract, 5, (err, result) => {
